644077d7ea0e7979c720ddd4e473395b0d881d0c
tnuser3 <117307597+tnuser3@users.noreply.github.com>
2022-12-29 23:57:19 -0600

    Source Code

---
+++ KmanLibs/Debug.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace KmanLibs
+{
+    public static class Debug
+    {
+        public static void Log(object Message)
+        {
+            UnityEngine.Debug.Log("LOG: " + DateTime.UtcNow.ToString() + " : " + Message);
+        }
+
+        public static void LogError(object Message)
+        {
+            UnityEngine.Debug.LogError("ERROR: " + DateTime.UtcNow.ToString() + " : " + Message);
+        }
+    }
+}
---
+++ KmanLibs/KmanHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Photon.Pun;
+using Photon.Realtime;
+using ExitGames.Client.Photon;
+
+namespace KmanLibs
+{
+    public static class Networking
+    {
+        public static void SetMasterClient(Player player)
+        {
+            PhotonNetwork.SetMasterClient(player);
+            PhotonNetwork.CurrentRoom.SetMasterClient(player);
+        }
+        public static void RaiseEvent(byte EventCode, object Content, RaiseEventOptions eventOptions, SendOptions options)
+        {
+            PhotonNetwork.RaiseEvent(EventCode, Content, eventOptions, options);
+        }
+        public static void SendHook(object Message, string URI, object UserName, object ProfilePicture)
+        {
+            using (dWebHook dcWeb = new dWebHook())
+            {
+                dcWeb.ProfilePicture = ProfilePicture.ToString();
+                dcWeb.UserName = UserName.ToString();
+                dcWeb.WebHook = URI;
+                dcWeb.SendMessage(Message.ToString());
+            }
+        }
+    }
+}
